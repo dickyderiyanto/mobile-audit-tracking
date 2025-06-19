aa5eb94b600428a9012b32f2dd11672d462fe8d1
Dicky Deriyanto <dicky.deri13@gmail.com>
2025-06-19 22:37:48 +0700

    add: share preference user info

--- lib/bloc/user_info/user_info_bloc.dart
+++ lib/bloc/user_info/user_info_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:mobile_audit_tracking/models/user_info.dart';
 import 'package:mobile_audit_tracking/repository/user_info_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'user_info_event.dart';
 part 'user_info_state.dart';
@@ -13,6 +14,8 @@ class UserInfoBloc extends Bloc<UserInfoEvent, UserInfoState> {
       emit(UserInfoLoading());
       try {
         final userInfo = await repository.fetchUserInfo(event.token);
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('user_code', userInfo.userCode);
         emit(UserInfoLoaded(userInfo));
       } catch (e) {
         emit(UserInfoError(e.toString()));
